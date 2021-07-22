a7582743d22cc59d7d25ac9cd03b4be5e80cf84e
Olli Tapaninen <olli.tapaninen@icloud.com>
2021-07-22 14:33:16 +0300

    Initial Commit

---
+++ IfLetStoreTest/IfLetStoreTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IfLetStoreTestApp.swift
+//  IfLetStoreTest
+//
+//  Created by Olli Tapaninen on 22.7.2021.
+//
+
+import SwiftUI
+
+@main
+struct IfLetStoreTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
